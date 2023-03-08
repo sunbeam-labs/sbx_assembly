3b0353def77a91f289acf4532c58296022e885b8
ctbus <ctbushman@gmail.com>
2023-03-08 14:47:15 -0500

    Fix up final_filter

--- scripts/final_filter.py
+++ scripts/final_filter.py
@@ -1,12 +1,9 @@
-import shutil
-import sys
-from final_filter_f import parse_fasta, filter_seqs, write_fasta
+from sunbeamlib.parse import parse_fasta, write_fasta
 
-with open(snakemake.log[0], "w") as l:
-    with open(snakemake.input[0]) as f:
-        with open(f"{snakemake.input[0]}.{snakemake.params.len}f", "w") as g:
-            write_fasta(g, list(filter_seqs(parse_fasta(f), snakemake.params.len)))
-
-    shutil.copyfile(
-        f"{snakemake.input[0]}.{snakemake.params.len}f", snakemake.output[0]
-    )
+with open(snakemake.log[0], "w") as log:
+    with open(snakemake.input[0]) as f_in, open(snakemake.output[0], "w") as f_out:
+        for record in parse_fasta(f_in):
+            if len(record[1]) >= snakemake.params.len:
+                write_fasta(record, f_out)
+            else:
+                log.write(f"Filtered {record[0]} for being too short")
