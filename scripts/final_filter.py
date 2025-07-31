a755062516447cfb570d8b0dee025be0762cabc7
Ulthran <ctbushman@gmail.com>
2025-07-31 10:44:30 -0400

    Just do it right

--- scripts/final_filter.py
+++ scripts/final_filter.py
@@ -1,8 +1,23 @@
-try:
-    from sunbeam.bfx.parse import parse_fasta, write_fasta
-except ModuleNotFoundError:
-    # For compatibility with older versions of Sunbeam
-    from sunbeamlib.parse import parse_fasta, write_fasta
+from itertools import groupby
+from typing import Iterator, TextIO, Tuple
+
+
+def parse_fasta(f: TextIO) -> Iterator[Tuple[str, str]]:
+    faiter = (x[1] for x in groupby(f, lambda line: line[0] == ">"))
+
+    for header in faiter:
+        # drop the ">"
+        header_str = header.__next__()[1:].strip()
+
+        # join all sequence lines to one.
+        seq_str = "".join(s.strip() for s in faiter.__next__())
+
+        yield (header_str, seq_str)
+
+
+def write_fasta(record: Tuple[str, str], f: TextIO) -> None:
+    f.write(f">{record[0]}\n")
+    f.write(f"{record[1]}\n")
 
 
 with open(snakemake.log[0], "w") as log:
